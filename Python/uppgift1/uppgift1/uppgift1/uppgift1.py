f981843a930ee6a489a50472152c0d6ecfe27958
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-06-08 00:23:08 +0200

    d
    d

--- Python/uppgift1/uppgift1/uppgift1/uppgift1.py
+++ Python/uppgift1/uppgift1/uppgift1/uppgift1.py
@@ -13,7 +13,7 @@ print(" Well you have a " + car_make + " of " + model + " and already from " + y
 # buy car code sett
 
 money = 1000
-sale = 3
+sale = 0
 menu = 0
 car1 = 250
 car_Name1 = "volvo v100"
@@ -22,7 +22,12 @@ car_Name2 = "volvo v200"
 car3 = 300
 car_Name3 = "volvo v300"
 
-while money > car1 and money > car2 and money > car3 :
+owned_car1 = {"Volvo v100", "Volvo V200", "Volvo V 300"}
+owned_car2 = {"Volvo V200", "Volvo V 300"}
+owned_car3 = {"Volvo V 300"}
+
+
+while money > car1 and money > car2 and money > car3 and menu != True :
     menu = int(input("Choose menu 1: to buy, 2 sale, 3 exit: "))
     if menu == 1:
        menu2 = 0
@@ -31,21 +36,68 @@ while money > car1 and money > car2 and money > car3 :
            money -= car1
            print(money)
            print(car_Name1)
+           sale += 1
            
        elif menu2 == 2: 
            money -= car2
            print(money)
            print(car_Name2)
+           sale += 1
 
        elif menu2 == 3:
             money -= car3
             print(money)
             print(car_Name3)
+
        elif menu2 == 4:
             print(money)
+            print(Sale)
+            menu == 0
          
        else :
             print("wrong keyboard input")
+    elif menu == 2 and sale < 1:
+       menu3 = 0
+       menu2 = int(input(" Do you want to sell? Volvo v100 (1), volvo v200, (3) volvo v300, (4) exit: " ))
+       if menu3 == 1 and sale < 1 :
+           money += car1
+           print(money)
+           print(car_Name1)
+           sale -= 1
+           
+       elif menu3 == 2 and sale < 1: 
+           money += car2
+           print(money)
+           print(car_Name2)
+
+       elif menu3 == 3 and sale < 1:
+            money += car3
+            print(money)
+            print(car_Name3)
+       elif menu3 == 4:
+            print(money)
+            print(sale)
+            menu == 0
+         
+       else :
+            print("wrong keyboard input or you dont own a car")
+
+    elif menu == 3:
+        menu = True
+
     else :
         print("wrong keyboard input")
 print("You have no more money")
+
+if sale == 1 :
+    print("Owned cars ",owned_car3)
+elif sale == 2:
+    print("Owned cars ",owned_car2)
+elif sale == 3:
+    print("Owned cars ",owned_car1)
+
+
+
+f = open('text.txt', 'r')
+print(f.name)
+f.close()
