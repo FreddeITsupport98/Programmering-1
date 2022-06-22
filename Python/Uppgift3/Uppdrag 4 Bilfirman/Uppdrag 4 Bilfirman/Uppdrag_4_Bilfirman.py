39e092f8f3483abd577f0fd332a0389d425f2e4a
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-06-22 23:49:14 +0200

    f
    f

--- Python/Uppgift3/Uppdrag 4 Bilfirman/Uppdrag 4 Bilfirman/Uppdrag_4_Bilfirman.py
+++ Python/Uppgift3/Uppdrag 4 Bilfirman/Uppdrag 4 Bilfirman/Uppdrag_4_Bilfirman.py
@@ -1,8 +1,9 @@
+from datetime import date
 from getkey import getkey, keys
 import replit
 
 def staff():
-    print("Start to write in staff")
+    print("Start to write to logg staff time")
 
     staff = open('staff.txt', 'a')
     employee = input('Enter your name to check in: ')
@@ -52,17 +53,17 @@ def service():
     service_time = input('Service repair time in weeks: ')
 
     service.write(' $ ' + car_price + ',' + ' Model: ' + car_model + ' Manufacture date: ' + manufacture_date + ' , ' + owner + 'service time:'+  service_time +'\n')
-    service.close
+    service.close()
 
 def service_read():
-    service = open('ticket.txt', 'r')
+    service = open('service.txt', 'r')
     print(service.read(), 'checkout')
     service.close()
 
 def agreement():
     print('Welcome to user agreement here you sign the contract for leasing or purchase is made')
     
-    menu = input('Do you want sign contract?: Yes or No: ')
+    menu = input('Do you want sign contract?: (Yes) register new warranty or (No) i already have one: ')
     if menu == 'yes':
         contract = open('agreement.txt', 'a')
         name_owner = input('Write your name: ')
@@ -78,6 +79,7 @@ def agreement():
         contract.write('Dealer ship signing contrak \n')
         contract.write('Her by i purchase this car by accepting warranty and lease time \n')
         contract.write('Here by i accept warranty-policy and leasing license \n')
+        contract.write('\n')
         contract.write('Name and surname\n')
         contract.write(name_owner +'\n')
         contract.write('***********\n')
@@ -123,18 +125,159 @@ def agreement_read():
     elif menu == 'no':
         print('Exiting... press enter.')
 
+def managment():
+    print('Welcome to managment console...')
+    
+    file = open('agreement.txt', 'a')
+    pinAccess = 0
+    name = input('Enter your name: ')
+    print(' Welcome ' + name)
+    while pinAccess < 3:
+        pin = 1234
+        userPin = int(input('Please ogin with your pin: '))
+        if pin != userPin:
+            print("Wrong input")
+            pinAccess += 1
+            print("Numbers of tries: ", pinAccess)
+            continue
+        elif pin == userPin:
+            print('Welcome to the Administration console: ' + name)
+            pinAccess = 3
+    
+
+    while True:
+        menu = int(input('Welcome to the console:\n ' + name + '\nMenu: staff logg book (1) \n' + 'Apply violation to staff (2)\n' + 'Apply warning to staff (3) \n' + 'Exit console (4)\n: ' + 'Input: '))
+        if menu == 1:
+            print('Load staff logg book')
+            menu1 = input('Load file: Yes or No: ')
+            if menu1 == 'yes':
+                print('Loading file...')
+                staff = open('staff.txt','r')
+                print(staff.read())
+                staff.close()
+                menu3 = input('Do you want to append staff list?: Yes or No: ')
+                if menu3 == 'yes':
+                    print('Appending file...')
+                    staff = open('staff.txt', 'a')
+                    employee = input('Enter your name to check in: ')
+                    staffId = input('enter staff id: ')
+                    time = input('Enter date: ')
+
+                    staff.write(employee + ',' + ' Welcome staff ID: ' + staffId + ',' + time + '\n')
+                    print(' Welcome ' + employee + ' Your staff ID: ' + staffId + ',' + time + ' data is stored in server ')
+                    print('Done file is saved')
+                    staff.close()
+                elif menu3 == 'no':
+                    print('Exiting...')
+                    
+            elif menu1 == 'no':
+                print('exiting')
+        elif menu == 2:
+            print('Welcome to staff managenent: Violation')
+            menu4 = input('Do you want to load or append violation?: load or append: ')
+            if menu4 == 'load':
+                print('Loading previus violations')
+                violation = open('violation.txt', 'r')
+                print(violation.read())
+            elif menu4 == 'append':
+                violation = open('violation.txt', 'a')
+                name_violation = input('Enter person name and surname: ')
+                datum = input('Enter a date when violation occur')
+                reason = input('Reason for violation: ')
+                length = input('Length of punishment: ')
+                cost = input('Amount of damaged need to cost: ')
+                boss = input('Boss signature: ')
+
+                violation.write('\n')
+                violation.write('***********\n')
+                violation.write('This file is for staff violation \n')
+                violation.write('here by i accept my punishement for violating rules of company \n')
+                violation.write('There for i accept punishiement prosucuted by company policy rules \n')
+                violation.write('\n')
+                violation.write('Name and surname\n')
+                violation.write(name_violation +'\n')
+                violation.write('***********\n')
+                violation.write('\n')
+                violation.write('datum\n')
+                violation.write(datum +'\n')
+                violation.write('***********\n')
+                violation.write('\n')
+                violation.write('reason\n')
+                violation.write(reason +'\n')
+                violation.write('***********\n')
+                violation.write('\n')
+                violation.write('Lenght follow by punishment\n')
+                violation.write(length+'\n')
+                violation.write('***********\n')
+                violation.write('Cost\n')
+                violation.write(cost+'\n')
+                violation.write('***********\n')
+                violation.write('\n')
+                violation.write('Signed by manager\n')
+                violation.write(boss +'\n')
+                violation.write('***********\n')
+                violation.close()
+        elif menu == 3:
+            print('Welcome to staff managenent: Warning')
+            menu4 = input('Do you want to load or append Warning?: load or append: ')
+            if menu4 == 'load':
+                print('Loading previus warning')
+                warning = open('warning.txt', 'r')
+                print(warning.read())
+            elif menu4 == 'append':
+                warning = open('warning.txt', 'a')
+                name_warning = input('Enter person name and surname: ')
+                datum = input('Enter a date when warning occur')
+                reason = input('Reason for warning: ')
+                length = input('Length of warning remains: ')
+                cost = input('cost: ')
+                boss = input('Boss signature: ')
+
+                warning.write('\n')
+                warning.write('***********\n')
+                warning.write('This file is for staff warning \n')
+                warning.write('here by i accept my warning for violating rules of company \n')
+                warning.write('There for if i repeat i must accept punishment prosucuted by company policy rules \n')
+                warning.write('\n')
+                warning.write('Name and surname\n')
+                warning.write(name_warning +'\n')
+                warning.write('***********\n')
+                warning.write('\n')
+                warning.write('datum\n')
+                warning.write(datum +'\n')
+                warning.write('***********\n')
+                warning.write('\n')
+                warning.write('reason\n')
+                warning.write(reason +'\n')
+                warning.write('***********\n')
+                warning.write('\n')
+                warning.write('Lenght follow by warning\n')
+                warning.write(length+'\n')
+                warning.write('***********\n')
+                warning.write('Cost\n')
+                warning.write(cost+'\n')
+                warning.write('***********\n')
+                warning.write('\n')
+                warning.write('Signed by manager\n')
+                warning.write(boss +'\n')
+                warning.write('***********\n')
+                warning.close()
+        elif menu == 4:
+            print('exiting administation console...')
+            break
+
+
 
 
 
 menu = 0
 car = 0
-money = 1000
+money = 100000
 cars = {'volvo 2005', 'volvo 2010', 'volvo 2015' }
 
 while True:
-    array = {'Register buyers*', 'Handle cars*','Perform service, repairs and warranty matters', 'Manage staff' }
-    print(array)
-    menu = int(input('Whats today service: staff login 1, register as buyers 2, warranty check 3:' ))
+    print(money, 'Kr as founds')
+    menu = int(input('Whats today service \n: staff login (1) \n, register as buyers (2)\n, warranty check (3)\n, staff managemnt(Admin) (4) \n, exit the program (5)\n' + 'Input: ' ))
     if menu == 1:
         menu2 = 0
         menu2 = int(input('Do you want to check in: 1: for yes or 2: for manager read logg: '))
@@ -144,6 +287,7 @@ while True:
         elif menu2 == 2:
             print('This is for manager to read log')
             staff_read()
+
     elif menu == 2:
          print('Register your purchase or purchese a car')
          menu3 = 0
@@ -169,18 +313,21 @@ while True:
             if menu5 == 'yes':
                 print('You need to purchase car ticket format will hand out and warranty license need to be filled')
                 ticket()
+                money -= 70000
+                print(money)
                 print('after you finnished sign ticket for purches next is sign warranty license')
                 agreement()
             elif menu5 == 'no':
                 print(cars)
                 print('here is aviable cars to purchase')
+
     elif menu == 3:
         print('Welcome to warranty checkout, here you can check if you are listed in warranty')
         menu7 = input("Do you want us to check your warranty: Yes or no: ")
         if menu7 == 'yes':
             print('Let us take a look at warranty database in your name')
             agreement_read()
-            menu8 = input('Your name show in database? Yes or no: ')
+            menu8 = input('Does your name show in database? Yes or no: ')
             if menu8 == 'yes':
                 print('Do you seek maintenance')
                 menu9 = input('Yes or No i want to read my service status: ')
@@ -189,25 +336,32 @@ while True:
                     service_read()
                     print('Next warranty')
                     agreement_read() 
-                    menu11 = input('Do you want to register your warranty and service?: yes or no: ')
-                    if menu11 == 'yes':
+                    print('Next is proof of purchase the car')
+                    ticket_read()
+                    menu11 = input('Do you want to register your warranty and service it cost 500 kr?: yes or no: ')
+                    if menu11 == 'yes' and money > 500:
+                        money -= 500
                         print('Alright service registering')
                         service()
-                        print('Then warranty')
+                        print('Then warranty registering')
                         agreement()
-                        print('We let you know when car is finished from service')
+                        print('We let you know when car is finished from service!')
                     elif menu11 == 'no':
                         print('Bye')
+                    else: 
+                        print('Not enough founds to apply service')
                 elif menu9 == 'no':
                     print('Alright let see service status')
                     service_read()
-                    menu10 = input('Do you want to register at service?: yes or no: ')
-                    if menu == 'yes':
-                        print()
+                    menu10 = input('Do you want to register at service 500kr cost it?: yes or no: ')
+                    if menu == 'yes' and money > 500:
+                        print('Let us see how we can help you att service registering')
                         service()
-
+                        money -= 500
                     elif menu10 == 'no':
-                        print('Goodbye')
+                        print('Goodbye thank you for your patience')
+                    else: 
+                        print('Not enough founds to apply service')
             elif menu8 == 'no':
                 print('Do you want to register your warranty?')
                 menu12 = input('Yes or no: ')
@@ -218,7 +372,15 @@ while True:
                     print('Bye')
         elif menu7 == 'no':
             print('Get back any time!')
+
     elif menu == 4:
-        print('4')
+        print('This section is for staff management')
+        
+        menu13 =input('Welcome do you want to login administration console? yes or no: ')
+        if menu13 == 'yes':
+            print('loading into console...')
+            managment()
+        elif menu13 == 'no':
+            print('Call us if you need to use administator console')
     elif menu == 5:
         break
